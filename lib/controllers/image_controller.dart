7ad79638dcb29988ac9112dfe3630a8efc616b27
Rahul <rahulsangwan797@gmail.com>
2025-05-24 18:12:05 +0530

    fixed loading error

--- lib/controllers/image_controller.dart
+++ lib/controllers/image_controller.dart
@@ -16,28 +16,24 @@ class ImageController extends GetxController {
   final predictionController = Get.find<PredictionController>();
 
   Future<void> pickFromGallery() async {
-    predictionController.isLoading.value = true;
     final ImagePicker picker = ImagePicker();
     try {
       XFile? image = await picker.pickImage(source: ImageSource.gallery);
       if (image == null) return;
-      final bytes = await image.readAsBytes();
 
+      predictionController.isLoading.value = true;
+      final bytes = await image.readAsBytes();
       final originalImage = img.decodeImage(bytes);
       if (originalImage == null) throw Exception('Could not decode image');
-
       final resizedImage = img.copyResize(originalImage, width: IMAGE_WIDTH);
-
       final resizedBytes = img.encodeJpg(resizedImage, quality: 90);
 
       final tempDir = Directory.systemTemp;
       final fileName = 'resized_${image.name}';
       final resizedFile = File('${tempDir.path}/$fileName');
-
       await resizedFile.writeAsBytes(resizedBytes);
 
       image = XFile(resizedFile.path);
-
       selectedImage.value = image;
       final Map<String, dynamic> predictedVal = await predictionController.predict(
         File(image.path),
@@ -61,29 +57,23 @@ class ImageController extends GetxController {
   Future<void> captureImage() async {
     if (captureDisable.value) return;
     captureDisable.value = true;
-    predictionController.isLoading.value = true;
-
     try {
       XFile? photo = await cameraService.takePicture();
-
       if (photo == null) return;
-      final bytes = await photo.readAsBytes();
 
+      predictionController.isLoading.value = true;
+      final bytes = await photo.readAsBytes();
       final originalImage = img.decodeImage(bytes);
       if (originalImage == null) throw Exception('Could not decode image');
-
       final resizedImage = img.copyResize(originalImage, width: IMAGE_WIDTH);
-
       final resizedBytes = img.encodeJpg(resizedImage, quality: 90);
 
       final tempDir = Directory.systemTemp;
       final fileName = 'resized_${photo.name}';
       final resizedFile = File('${tempDir.path}/$fileName');
-
       await resizedFile.writeAsBytes(resizedBytes);
 
       photo = XFile(resizedFile.path);
-
       selectedImage.value = photo;
       final Map<String, dynamic> predictedVal = await predictionController.predict(
         File(photo.path),
